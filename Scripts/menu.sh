ad5b6eba3026a3116961066627f5cebff1339408
ArtDr12 <artdr09s@gmail.com>
2025-09-03 08:32:24 +0300

    menu 3

--- Scripts/menu.sh
+++ Scripts/menu.sh
@@ -6,6 +6,7 @@ theme="$HOME/.config/rofi/config.rasi"
 # Options
 wallpaper='Change Wallpaper'
 powermenu='Manage Session'
+update='System Update'
 
 # Rofi CMD
 rofi_cmd() {
@@ -16,7 +17,7 @@ rofi_cmd() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$wallpaper\n$powermenu" | rofi_cmd
+	echo -e "$wallpaper\n$powermenu\n$update" | rofi_cmd
 }
 
 # Execute Command
@@ -25,6 +26,8 @@ run_cmd() {
 		for a in $HOME/.config/hypr/backgrounds/*; do echo -en "$(basename $a)\0icon\x1f$a\n"; done | swww img $HOME/.config/hypr/backgrounds/$(rofi -dmenu -p "Choose the background" -theme-str 'inputbar { enabled: false; }') --transition-type outer --transition-step 15 && notify-send -i preferences-desktop-theme "Wallpaper changed"
   elif [[ $1 == '--powermenu' ]]; then
      $HOME/Scripts/powermenu.sh
+  elif [[ $1 == '--update' ]]; then 
+      kitty $HOME/Scripts/update.sh
   fi
 }
 
@@ -36,5 +39,8 @@ case ${chosen} in
         ;;
     $powermenu)
 		run_cmd --powermenu
+        ;;
+    $update)
+    run_cmd --update 
         ;;
 esac
